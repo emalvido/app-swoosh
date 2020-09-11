1d1f251236c4c5b29baed529e3a81d3369fd2a22
emalvido <emalvido2@hotmail.com>
2020-09-11 14:18:48 +0200

    first version of the front screen

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Eduardo Malvido on 9/11/20.
+//  Copyright © 2020 Eduardo Malvido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
